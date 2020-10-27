0173eb95e6b01245afb50ef9912c8d70ea9dfe59
Lucas Pagliarini <53023632+lucaspag89@users.noreply.github.com>
2020-10-27 17:32:26 -0300

    New models

--- SmartSchool.WebAPI/Models/AlunoDisciplina.cs
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -1,13 +1,19 @@
+using System;
+
 namespace SmartSchool.WebAPI.Models
 {
   public class AlunoDisciplina
   {
     public AlunoDisciplina() { }
-    public AlunoDisciplina(int alunoId, int disciplinaId)
+    public AlunoDisciplina(int alunoId,
+                           int disciplinaId)
     {
       this.AlunoId = alunoId;
       this.DisciplinaId = disciplinaId;
     }
+    public DateTime DataIni { get; set; } = DateTime.Now;
+    public DateTime? DataFim { get; set; }
+    public int? Nota { get; set; }
     public int AlunoId { get; set; }
     public Aluno Aluno { get; set; }
     public int DisciplinaId { get; set; }
