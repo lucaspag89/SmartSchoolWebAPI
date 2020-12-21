1752f3abfe020c0dbf3704ae4322bcffff40e096
Lucas Pagliarini <lucas_pagliarini@hotmail.com>
2020-12-21 14:35:10 -0300

    Swagger

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -9,106 +9,132 @@ using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class AlunoController : ControllerBase
-  {
-    public readonly IRepository _repo;
-    public readonly IMapper _mapper;
-
-    public AlunoController(IRepository repo, IMapper mapper)
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
     {
-      _repo = repo;
-      _mapper = mapper;
+        public readonly IRepository _repo;
+        public readonly IMapper _mapper;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repo"></param>
+        /// <param name="mapper"></param>
+        public AlunoController(IRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Método para retornar todos os alunos.
+        /// </summary>
+        /// <returns></returns>
+        // api/aluno
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var alunos = _repo.GetAllAlunos(true);
+
+            return Ok(_mapper.Map<IEnumerable<AlunoDTO>>(alunos));
+        }
+
+        /// <summary>
+        /// Método responsável por retonar apenas um único AlunoDTO.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("getRegister")]
+        public IActionResult GetRegister()
+        {
+            return Ok(new AlunoRegistrarDto());
+        }
+
+        /// <summary>
+        /// Método responsável por retonar apenas um Aluno por meio do Código ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // api/aluno/(id)
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = _repo.GetAlunoById(id, false);
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado!!");
+
+            var alunoDto = _mapper.Map<AlunoDTO>(aluno);
+
+            return Ok(alunoDto);
+        }
+
+        // api/aluno
+        [HttpPost]
+        public IActionResult Post(AlunoRegistrarDTO model)
+        {
+            var aluno = _mapper.Map<Aluno>(model);
+
+            _repo.Add(aluno);
+            if (_repo.SaveChanges())
+            {
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
+            };
+
+            return BadRequest("Aluno não cadastrado!");
+        }
+
+        // api/aluno
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, AlunoRegistrarDTO model)
+        {
+            var aluno = _repo.GetAlunoById(id);
+            if (aluno == null) return BadRequest("Aluno não encontrado!");
+
+            _mapper.Map(model, aluno);
+
+            _repo.Update(aluno);
+            if (_repo.SaveChanges())
+            {
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
+            };
+
+            return BadRequest("Não foi possível atualizar os dados do aluno!");
+        }
+
+        // api/aluno
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, AlunoRegistrarDTO model)
+        {
+            var aluno = _repo.GetAlunoById(id, false);
+            if (aluno == null) return BadRequest("Aluno não encontrado!");
+
+            _mapper.Map(model, aluno);
+
+            _repo.Update(aluno);
+            if (_repo.SaveChanges())
+            {
+                return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
+            };
+
+            return BadRequest("Não foi possível atualizar os dados do aluno!");
+        }
+
+        // api/aluno
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var aluno = _repo.GetAlunoById(id, false);
+            if (aluno == null) return BadRequest("Aluno não encontrado!");
+
+            _repo.Delete(aluno);
+            if (_repo.SaveChanges())
+            {
+                return Ok("Registro excluído");
+            };
+
+            return BadRequest("Não foi possível excluir o registro do aluno!");
+        }
     }
-
-    // api/aluno
-    [HttpGet]
-    public IActionResult Get()
-    {
-      var alunos = _repo.GetAllAlunos(true);
-
-      return Ok(_mapper.Map<IEnumerable<AlunoDTO>>(alunos));
-    }
-
-    // api/aluno/(id)
-    [HttpGet("{id}")]
-    public IActionResult GetById(int id)
-    {
-      var aluno = _repo.GetAlunoById(id, false);
-      if (aluno == null) return BadRequest("O Aluno não foi encontrado!!");
-
-      var alunoDto = _mapper.Map<AlunoDTO>(aluno);
-
-      return Ok(alunoDto);
-    }
-
-    // api/aluno
-    [HttpPost]
-    public IActionResult Post(AlunoRegistrarDTO model)
-    {
-      var aluno = _mapper.Map<Aluno>(model);
-
-      _repo.Add(aluno);
-      if (_repo.SaveChanges())
-      {
-        return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
-      };
-
-      return BadRequest("Aluno não cadastrado!");
-    }
-
-
-    // api/aluno
-    [HttpPut("{id}")]
-    public IActionResult Put(int id, AlunoRegistrarDTO model)
-    {
-      var aluno = _repo.GetAlunoById(id);
-      if (aluno == null) return BadRequest("Aluno não encontrado!");
-
-      _mapper.Map(model, aluno);
-
-      _repo.Update(aluno);
-      if (_repo.SaveChanges())
-      {
-        return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
-      };
-
-      return BadRequest("Não foi possível atualizar os dados do aluno!");
-    }
-
-    // api/aluno
-    [HttpPatch("{id}")]
-    public IActionResult Patch(int id, AlunoRegistrarDTO model)
-    {
-      var aluno = _repo.GetAlunoById(id, false);
-      if (aluno == null) return BadRequest("Aluno não encontrado!");
-
-      _mapper.Map(model, aluno);
-
-      _repo.Update(aluno);
-      if (_repo.SaveChanges())
-      {
-        return Created($"/api/aluno/{model.Id}", _mapper.Map<AlunoDTO>(aluno));
-      };
-
-      return BadRequest("Não foi possível atualizar os dados do aluno!");
-    }
-
-    // api/aluno
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-      var aluno = _repo.GetAlunoById(id, false);
-      if (aluno == null) return BadRequest("Aluno não encontrado!");
-
-      _repo.Delete(aluno);
-      if (_repo.SaveChanges())
-      {
-        return Ok("Registro excluído");
-      };
-
-      return BadRequest("Não foi possível excluir o registro do aluno!");
-    }
-  }
 }
--- SmartSchool.WebAPI/Startup.cs
+++ SmartSchool.WebAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
@@ -16,67 +18,85 @@ using SmartSchool.WebAPI.Data;
 
 namespace SmartSchool.WebAPI
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<SmartSchoolContext>(
-          context => context.UseSqlite(Configuration.GetConnectionString("SmartSchoolContext")));
-
-      services.AddControllers()
-              .AddNewtonsoftJson(opt =>
-                opt.SerializerSettings.ReferenceLoopHandling =
-                Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-      services.AddScoped<IRepository, Repository>();
-
-      services.AddSwaggerGen(opt =>
-      {
-        opt.SwaggerDoc(
-          "smartschoolapi",
-          new Microsoft.OpenApi.Models.OpenApiInfo()
-          {
-            Title = "SmartSchool API",
-            Version = "1.0"
-          });
-      });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      // app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseSwagger()
-        .UseSwaggerUI(opt =>
+        public Startup(IConfiguration configuration)
         {
-          opt.SwaggerEndpoint("http://localhost:5000/swagger/smartschoolapi/swagger.json", "smartschoolapi");
-          opt.RoutePrefix = "";
-        });
+            Configuration = configuration;
+        }
 
-      // app.UseAuthorization();
+        public IConfiguration Configuration { get; }
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SmartSchoolContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("SmartSchoolContext")));
+
+            services.AddControllers()
+                    .AddNewtonsoftJson(opt =>
+                      opt.SerializerSettings.ReferenceLoopHandling =
+                      Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IRepository, Repository>();
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc(
+                    "smartschoolapi",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "SmartSchool API",
+                        Version = "1.0",
+                        TermsOfService = new Uri("http://termsexample.com"),
+                        Description = "A descrição da WebAPI da SmartSchool",
+                        License = new Microsoft.OpenApi.Models.OpenApiLicense
+                        {
+                            Name = "SmartSchool License",
+                            Url = new Uri("http://example.com")
+                        },
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                            Name = "Lucas Pagliarini",
+                            Email = "example@example.com",
+                            Url = new Uri("http://contact.com")
+                        }
+                    });
+
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+                opt.IncludeXmlComments(xmlCommentsFullPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger()
+              .UseSwaggerUI(opt =>
+              {
+                  opt.SwaggerEndpoint("http://localhost:24835/swagger/smartschoolapi/swagger.json", "smartschoolapi");
+                  opt.RoutePrefix = "";
+              });
+
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-  }
 }
